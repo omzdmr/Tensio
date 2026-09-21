06289c108419d200297d3ac5e0ccc3256dc9d010
omzdmr <154471764+omzdmr@users.noreply.github.com>
2026-09-22 02:25:47 +0800

    feat: add lib/core/design/tensio_spacing.dart

---
+++ lib/core/design/tensio_spacing.dart
@@ -0,0 +1,10 @@
+abstract final class TensioSpacing {
+  static const x1 = 4.0;
+  static const x2 = 8.0;
+  static const x3 = 12.0;
+  static const x4 = 16.0;
+  static const x5 = 20.0;
+  static const x6 = 24.0;
+  static const x8 = 32.0;
+  static const x10 = 40.0;
+}
